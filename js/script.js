c9c396dd992e31e57a650596b22112e5eb7ab467
David Carlisle <dave_0021@yahoo.com>
2020-08-19 16:10:24 -0500

    Update stripe api

--- js/script.js
+++ js/script.js
@@ -127,9 +127,3 @@ searchButton.addEventListener("click", (e) => {
       console.log("error");
     });
 });
-
-// Set your publishable key: remember to change this to your live publishable key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-// var stripe = Stripe("pk_test_7OnQMNYvV0yDCxyuNk3Ep1sC00Z0byTNPl");
-var stripe = Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
-var elements = stripe.elements();
---
+++ js/stripe.js
@@ -0,0 +1,5 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+// var stripe = Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+var stripe = Stripe("pk_test_7OnQMNYvV0yDCxyuNk3Ep1sC00Z0byTNPl");
+var elements = stripe.elements();
