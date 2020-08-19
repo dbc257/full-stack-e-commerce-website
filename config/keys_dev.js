4807b473a1461f1614568930c91a94888f38d3d2
David Carlisle <dave_0021@yahoo.com>
2020-08-19 15:14:28 -0500

    Update stripe api keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stripePublishableKey: "pk_test_TYooMQauvdEDq54NiTphI7jx",
-  stripeSecretKey: "sk_test_4TlD0jJVFwWmhRltBqGMBHQd00bpMuPH3u",
+  stripePublishableKey: "pk_live_usJXjANlMlkuguoq0CRNtWlv00OEKZ4ihM",
+  stripeSecretKey: "sk_live_0fjuLZgucuFZ79MF1wCLLQXU00bmYzR5mT",
 };
+// module.exports = {
+//   stripePublishableKey: "pk_test_TYooMQauvdEDq54NiTphI7jx",
+//   stripeSecretKey: "sk_test_4TlD0jJVFwWmhRltBqGMBHQd00bpMuPH3u",
+// };
