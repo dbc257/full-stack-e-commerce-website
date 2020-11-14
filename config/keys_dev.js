5ce08aadacf75f277b7191f82aa498d55a14e2af
David Carlisle <davidcarlisle@pop-os.localdomain>
2020-11-13 21:08:19 -0600

    deleted extra files

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stripePublishableKey: "pk_test_7OnQMNYvV0yDCxyuNk3Ep1sC00Z0byTNPl",
-  stripeSecretKey: "sk_test_4TlD0jJVFwWmhRltBqGMBHQd00bpMuPH3u",
-};
--- config/keys_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  STRIPE_TEST_KEY: process.env.STRIPE_TEST_KEY,
-  STRIPE_SECRET_TEST_KEY: process.env.STRIPE_SECRET_TEST_KEY,
-};
